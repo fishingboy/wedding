afd07f94eea3d20511f67d234f3ae1a986808c71
Leo.Kuo <et282523@hotmail.com>
2018-03-28 00:19:29 +0800

    加上 database-sample.php

---
+++ application/config/database-sample.php
@@ -0,0 +1,21 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_record = TRUE;
+$active_group = 'db';
+
+$db['db']['hostname'] = "127.0.0.1";
+$db['db']['username'] = "homestead";
+$db['db']['password'] = "secret";
+$db['db']['port']     = 3306;
+$db['db']['database'] = "wedding";
+$db['db']['dbdriver'] = "mysqli";
+$db['db']['dbprefix'] = "";
+$db['db']['pconnect'] = FALSE;
+$db['db']['db_debug'] = TRUE;
+$db['db']['cache_on'] = FALSE;
+$db['db']['cachedir'] = "";
+$db['db']['char_set'] = "utf8mb4";
+$db['db']['dbcollat'] = "utf8mb4_unicode_ci";
+$db['db']['swap_pre'] = '';
+$db['db']['autoinit'] = TRUE;
+$db['db']['stricton'] = FALSE;
