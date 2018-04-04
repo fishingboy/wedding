9666d222aaf110eb9f7318bf044fc282e54b27d9
Leo.Kuo <et282523@hotmail.com>
2018-04-04 12:15:13 +0800

    修正後台

--- application/views/index2_view.php
+++ application/views/index2_view.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>文彬＆昱伊婚禮邀請</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=2, shrink-to-fit=no">
     <!-- css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/bootstrap-grid.css" />
