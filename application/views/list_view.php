3bf3509fe9a579cea66948e65dc5361c96c3f653
Leo.Kuo <et282523@hotmail.com>
2018-04-05 21:35:59 +0800

    修正後台列表

--- application/views/list_view.php
+++ application/views/list_view.php
@@ -7,7 +7,7 @@
 <script src="/third-party/bootstrap/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <style>
     .base {min-width: 800px;}
-    .td-text {width:150px;}
+    .td-text {width:200px;}
 </style>
 </head>
 <body>
